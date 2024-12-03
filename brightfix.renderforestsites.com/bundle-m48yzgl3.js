9856c173b65230328d1af5c7e53052a96a09775e
Jacques0106 <61617419+Jacques0106@users.noreply.github.com>
2024-12-04 00:54:03 +0200

    updated .js

--- brightfix.renderforestsites.com/bundle-m48yzgl3.js
+++ brightfix.renderforestsites.com/bundle-m48yzgl3.js
@@ -19682,7 +19682,16 @@
                 I = (0, t.useCallback)((function() {
                     s("")
                 }), []);
-            return t.createElement(t.Fragment, null, t.createElement($d, null, t.createElement(nf, null, t.createElement(rf, null,), t.createElement(ef, null,)),),)
+            return t.createElement(t.Fragment, null, t.createElement($d, null, t.createElement(nf, null, t.createElement(rf, null,), t.createElement(ef, null,)),), r && t.createElement(df, {
+                onClose: I,
+                style: {
+                    maxWidth: "400px"
+                }
+            }, t.createElement(lp, {
+                message: u,
+                icon: f,
+                title: a
+            })))
         };
         gp.meta = lf();
         const mp = gp;
