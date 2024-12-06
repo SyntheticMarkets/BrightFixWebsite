0052305c5d9ee5b062d8fba43f3f4773017a94d0
Jacques0106 <61617419+Jacques0106@users.noreply.github.com>
2024-12-06 11:51:16 +0200

    update logo

---
+++ brightfix.renderforestsites.com/builtIcons-m4ciury8.js
@@ -0,0 +1,298 @@
+/*! For license information please see builtIcons-m4ciury8.js.LICENSE.txt */
+(() => {
+    var e = {
+            625: (e, t, r) => {
+                var o = {
+                    "./arrow_small_bottom.svg": 30,
+                    "./arrow_small_top.svg": 129,
+                    "./sortbyalphabetatoz-complexity-3.svg": 919
+                };
+
+                function n(e) {
+                    var t = a(e);
+                    return r(t)
+                }
+
+                function a(e) {
+                    if (!r.o(o, e)) {
+                        var t = new Error("Cannot find module '" + e + "'");
+                        throw t.code = "MODULE_NOT_FOUND", t
+                    }
+                    return o[e]
+                }
+                n.keys = function() {
+                    return Object.keys(o)
+                }, n.resolve = a, e.exports = n, n.id = 625
+            },
+            858: e => {
+                function t(e) {
+                    var t = new Error("Cannot find module '" + e + "'");
+                    throw t.code = "MODULE_NOT_FOUND", t
+                }
+                t.keys = () => [], t.resolve = t, t.id = 858, e.exports = t
+            },
+            316: e => {
+                function t(e) {
+                    var t = new Error("Cannot find module '" + e + "'");
+                    throw t.code = "MODULE_NOT_FOUND", t
+                }
+                t.keys = () => [], t.resolve = t, t.id = 316, e.exports = t
+            },
+            30: (e, t, r) => {
+                "use strict";
+                r.r(t), r.d(t, {
+                    default: () => i
+                });
+                var o, n = r(294);
+
+                function a() {
+                    return a = Object.assign || function(e) {
+                        for (var t = 1; t < arguments.length; t++) {
+                            var r = arguments[t];
+                            for (var o in r) Object.prototype.hasOwnProperty.call(r, o) && (e[o] = r[o])
+                        }
+                        return e
+                    }, a.apply(this, arguments)
+                }
+                const i = function(e) {
+                    return n.createElement("svg", a({
+                        xmlns: "http://www.w3.org/2000/svg",
+                        width: 32,
+                        height: 32,
+                        viewBox: "0 0 32 32"
+                    }, e), o || (o = n.createElement("path", {
+                        d: "M22.283 11.138L16 17.421l-6.283-6.283a1.619 1.619 0 00-2.283 0 1.619 1.619 0 000 2.283l7.417 7.433a1.618 1.618 0 002.3 0l7.417-7.433a1.619 1.619 0 000-2.283 1.619 1.619 0 00-2.283 0z"
+                    })))
+                }
+            },
+            129: (e, t, r) => {
+                "use strict";
+                r.r(t), r.d(t, {
+                    default: () => i
+                });
+                var o, n = r(294);
+
+                function a() {
+                    return a = Object.assign || function(e) {
+                        for (var t = 1; t < arguments.length; t++) {
+                            var r = arguments[t];
+                            for (var o in r) Object.prototype.hasOwnProperty.call(r, o) && (e[o] = r[o])
+                        }
+                        return e
+                    }, a.apply(this, arguments)
+                }
+                const i = function(e) {
+                    return n.createElement("svg", a({
+                        xmlns: "http://www.w3.org/2000/svg",
+                        width: 32,
+                        height: 32,
+                        viewBox: "0 0 32 32"
+                    }, e), o || (o = n.createElement("path", {
+                        d: "M24.567 20.862a1.619 1.619 0 000-2.283l-7.417-7.433a1.618 1.618 0 00-2.3 0l-7.417 7.433a1.619 1.619 0 000 2.283 1.619 1.619 0 002.283 0l6.283-6.283 6.283 6.283a1.619 1.619 0 002.283 0z"
+                    })))
+                }
+            },
+            919: (e, t, r) => {
+                "use strict";
+                r.r(t), r.d(t, {
+                    default: () => i
+                });
+                var o, n = r(294);
+
+                function a() {
+                    return a = Object.assign || function(e) {
+                        for (var t = 1; t < arguments.length; t++) {
+                            var r = arguments[t];
+                            for (var o in r) Object.prototype.hasOwnProperty.call(r, o) && (e[o] = r[o])
+                        }
+                        return e
+                    }, a.apply(this, arguments)
+                }
+                const i = function(e) {
+                    return n.createElement("svg", a({
+                        xmlns: "http://www.w3.org/2000/svg",
+                        width: 32,
+                        height: 32,
+                        viewBox: "0 0 32 32"
+                    }, e), o || (o = n.createElement("path", {
+                        d: "M.589 14.811a1.074 1.074 0 001.432-.467L4.398 9.6h6.146l2.377 4.744c.305.508.955.67 1.463.366a1.06 1.06 0 00.437-1.321l-6.4-12.8a1.065 1.065 0 00-1.899 0l-6.4 12.8a1.054 1.054 0 00.467 1.422zM7.467 3.454l2.001 4.013H5.465l2.001-4.013zM13.867 29.867H3.637l10.992-10.971c.305-.305.386-.762.223-1.158s-.549-.66-.985-.66h-12.8C.478 17.068 0 17.545 0 18.134s.477 1.067 1.067 1.067h10.23L.305 30.172c-.305.305-.386.762-.224 1.158s.549.66.985.66h12.8c.589 0 1.067-.477 1.067-1.067s-.477-1.056-1.067-1.056zM31.878 21.923a1.074 1.074 0 00-.945-.589h-3.2V1.067C27.733.478 27.256 0 26.666 0h-4.267c-.589 0-1.067.477-1.067 1.067v20.267h-3.2a1.067 1.067 0 00-.854 1.707l6.4 8.533a1.069 1.069 0 001.706 0l6.4-8.533c.244-.325.274-.762.091-1.117z"
+                    })))
+                }
+            },
+            418: e => {
+                "use strict";
+                var t = Object.getOwnPropertySymbols,
+                    r = Object.prototype.hasOwnProperty,
+                    o = Object.prototype.propertyIsEnumerable;
+
+                function n(e) {
+                    if (null == e) throw new TypeError("Object.assign cannot be called with null or undefined");
+                    return Object(e)
+                }
+                e.exports = function() {
+                    try {
+                        if (!Object.assign) return !1;
+                        var e = new String("abc");
+                        if (e[5] = "de", "5" === Object.getOwnPropertyNames(e)[0]) return !1;
+                        for (var t = {}, r = 0; r < 10; r++) t["_" + String.fromCharCode(r)] = r;
+                        if ("0123456789" !== Object.getOwnPropertyNames(t).map((function(e) {
+                                return t[e]
+                            })).join("")) return !1;
+                        var o = {};
+                        return "abcdefghijklmnopqrst".split("").forEach((function(e) {
+                            o[e] = e
+                        })), "abcdefghijklmnopqrst" === Object.keys(Object.assign({}, o)).join("")
+                    } catch (e) {
+                        return !1
+                    }
+                }() ? Object.assign : function(e, a) {
+                    for (var i, c, s = n(e), l = 1; l < arguments.length; l++) {
+                        for (var u in i = Object(arguments[l])) r.call(i, u) && (s[u] = i[u]);
+                        if (t) {
+                            c = t(i);
+                            for (var f = 0; f < c.length; f++) o.call(i, c[f]) && (s[c[f]] = i[c[f]])
+                        }
+                    }
+                    return s
+                }
+            },
+            408: (e, t, r) => {
+                "use strict";
+                var o = r(418),
+                    n = 60103;
+                if ("function" == typeof Symbol && Symbol.for) {
+                    var a = Symbol.for;
+                    n = a("react.element"), a("react.portal"), a("react.fragment"), a("react.strict_mode"), a("react.profiler"), a("react.provider"), a("react.context"), a("react.forward_ref"), a("react.suspense"), a("react.memo"), a("react.lazy")
+                }
+                "function" == typeof Symbol && Symbol.iterator;
+
+                function i(e) {
+                    for (var t = "https://reactjs.org/docs/error-decoder.html?invariant=" + e, r = 1; r < arguments.length; r++) t += "&args[]=" + encodeURIComponent(arguments[r]);
+                    return "Minified React error #" + e + "; visit " + t + " for the full message or use the non-minified dev environment for full errors and additional helpful warnings."
+                }
+                var c = {
+                        isMounted: function() {
+                            return !1
+                        },
+                        enqueueForceUpdate: function() {},
+                        enqueueReplaceState: function() {},
+                        enqueueSetState: function() {}
+                    },
+                    s = {};
+
+                function l(e, t, r) {
+                    this.props = e, this.context = t, this.refs = s, this.updater = r || c
+                }
+
+                function u() {}
+
+                function f(e, t, r) {
+                    this.props = e, this.context = t, this.refs = s, this.updater = r || c
+                }
+                l.prototype.isReactComponent = {}, l.prototype.setState = function(e, t) {
+                    if ("object" != typeof e && "function" != typeof e && null != e) throw Error(i(85));
+                    this.updater.enqueueSetState(this, e, t, "setState")
+                }, l.prototype.forceUpdate = function(e) {
+                    this.updater.enqueueForceUpdate(this, e, "forceUpdate")
+                }, u.prototype = l.prototype;
+                var p = f.prototype = new u;
+                p.constructor = f, o(p, l.prototype), p.isPureReactComponent = !0;
+                var d = null,
+                    h = Object.prototype.hasOwnProperty,
+                    v = {
+                        key: !0,
+                        ref: !0,
+                        __self: !0,
+                        __source: !0
+                    };
+                t.createElement = function(e, t, r) {
+                    var o, a = {},
+                        i = null,
+                        c = null;
+                    if (null != t)
+                        for (o in void 0 !== t.ref && (c = t.ref), void 0 !== t.key && (i = "" + t.key), t) h.call(t, o) && !v.hasOwnProperty(o) && (a[o] = t[o]);
+                    var s = arguments.length - 2;
+                    if (1 === s) a.children = r;
+                    else if (1 < s) {
+                        for (var l = Array(s), u = 0; u < s; u++) l[u] = arguments[u + 2];
+                        a.children = l
+                    }
+                    if (e && e.defaultProps)
+                        for (o in s = e.defaultProps) void 0 === a[o] && (a[o] = s[o]);
+                    return {
+                        $$typeof: n,
+                        type: e,
+                        key: i,
+                        ref: c,
+                        props: a,
+                        _owner: d
+                    }
+                }
+            },
+            294: (e, t, r) => {
+                "use strict";
+                e.exports = r(408)
+            }
+        },
+        t = {};
+
+    function r(o) {
+        var n = t[o];
+        if (void 0 !== n) return n.exports;
+        var a = t[o] = {
+            exports: {}
+        };
+        return e[o](a, a.exports, r), a.exports
+    }
+    r.d = (e, t) => {
+        for (var o in t) r.o(t, o) && !r.o(e, o) && Object.defineProperty(e, o, {
+            enumerable: !0,
+            get: t[o]
+        })
+    }, r.o = (e, t) => Object.prototype.hasOwnProperty.call(e, t), r.r = e => {
+        "undefined" != typeof Symbol && Symbol.toStringTag && Object.defineProperty(e, Symbol.toStringTag, {
+            value: "Module"
+        }), Object.defineProperty(e, "__esModule", {
+            value: !0
+        })
+    };
+    var o = {};
+    (() => {
+        "use strict";
+        r.r(o), r.d(o, {
+            ICONS: () => s,
+            ICON_NAMES: () => l
+        });
+        var e = function(e) {
+                return e.slice(2).replace(".svg", "")
+            },
+            t = function(t) {
+                return t.keys().map(e)
+            },
+            n = function(t) {
+                var r = {};
+                return t.keys().forEach((function(o) {
+                    var n = e(o);
+                    r[n] = t(o)
+                })), r
+            },
+            a = r(625),
+            i = r(858),
+            c = r(316),
+            s = {
+                glyphIcons: n(a),
+                outlineIcons: n(i),
+                socialIcons: n(c)
+            },
+            l = {
+                glyphIcons: t(a),
+                outlineIcons: t(i),
+                socialIcons: t(c)
+            }
+    })();
+    var n = self;
+    for (var a in o) n[a] = o[a];
+    o.__esModule && Object.defineProperty(n, "__esModule", {
+        value: !0
+    })
+})();
